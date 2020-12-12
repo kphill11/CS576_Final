c3cfac0e39b75f4eac0144c5f0b2fca0fc0e0298
kmecpp <kmecpp@gmail.com>
2020-12-11 23:04:27 -0500

    Add the rest of the code

--- src/final.go
+++ src/final.go
@@ -6,11 +6,16 @@ import (
 	"os/exec"
 )
 
-func main() {
+type Entry struct {
+	Key   string
+	Value int
+}
+
+func predict(json string) {
 	command := exec.Command("python", "src/test.py")
 
 	buffer := bytes.Buffer{}
-	buffer.Write([]byte("[2,[1,2,4,8]]"))
+	buffer.Write([]byte(json))
 	command.Stdin = &buffer
 
 	out, err := command.Output()
@@ -21,4 +26,47 @@ func main() {
 	}
 
 	fmt.Println(string(out))
+}
+
+func main2() {
+	//counties := readInput([]County{})
+	//fmt.Println(counties[0])
+
+	//a := newCounty("A")
+	//b := newCounty("B")
+	//c := newCounty("B")
+	//
+	//data := []County{a, b, c}
+
+	//m := map[string]int{
+	//	"something": 10,
+	//	"yo":        20,
+	//	"blah":      20,
+	//}
+	//
+	//var sortedCounties []Entry
+	//for k, v := range m {
+	//	sortedCounties = append(sortedCounties, Entry{k, v})
+	//}
+	//
+	//sort.Slice(sortedCounties, func(i, j int) bool {
+	//	return sortedCounties[i].Value > sortedCounties[j].Value
+	//})
+	//
+	//fmt.Println(sortedCounties)
+
+	//command := exec.Command("python", "src/test.py")
+	//
+	//buffer := bytes.Buffer{}
+	//buffer.Write([]byte("[2,[1,2,4,8]]"))
+	//command.Stdin = &buffer
+	//
+	//out, err := command.Output()
+	//
+	//if err != nil {
+	//	println(err.Error())
+	//	return
+	//}
+	//
+	//fmt.Println(string(out))
 }
